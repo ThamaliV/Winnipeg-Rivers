5c483723e67054a1646267f6256f9571b9404b3b
Thamali Vidanage <145381265+ThamaliV@users.noreply.github.com>
2026-09-11 15:30:37 -0500

    Update deploy.R

--- deploy.R
+++ deploy.R
@@ -1,6 +1,6 @@
 
 
-rsconnect::setAccountInfo(name='thamiv', token='4BD3B5F8F72D81205AC87E0701BF1515', secret='QWYWb/2r9MME+0/fk41MwHq8CUS7vOq/CwABgEKE')
+rsconnect::setAccountInfo(name='thamiv', token= '<>', secret='<>')
 
 library(rsconnect)
 library(prettyunits)
